3d933ecd85da995d768d948459da2e9c27cfa591
bala-amypo <bala@amypo.in>
2025-12-21 07:41:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/Visitor.java
+++ src/main/java/com/example/demo/entity/Visitor.java
@@ -52,27 +52,58 @@ public class Visitor {
         this.idProofNumber = idProofNumber;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId()
+    {
+         return id; 
+    }
+    public void setId(Long id){
+         this.id = id; 
+    }
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+    public String getFullName(){
+         return fullName; 
+    }
+    public void setFullName(String fullName){
+     this.fullName = fullName; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() {
+     return email; 
+     }
+    public void setEmail(String email) {
+     this.email = email; 
+     }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getPhone() {
+     return phone; 
+     }
 
-    public String getIdProofNumber() { return idProofNumber; }
-    public void setIdProofNumber(String idProofNumber) { this.idProofNumber = idProofNumber; }
+    public void setPhone(String phone){
+         this.phone = phone; 
+    }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
-    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+    public String getIdProofNumber(){
+         return idProofNumber; \
+    }
+    public void setIdProofNumber(String idProofNumber){
+         this.idProofNumber = idProofNumber; 
+    }
 
-    public List<Appointment> getAppointments() { return appointments; }
-    public void setAppointments(List<Appointment> appointments) { this.appointments = appointments; }
+    public LocalDateTime getCreatedAt(){
+         return createdAt; 
+    }
+    public void setCreatedAt(LocalDateTime createdAt){
+         this.createdAt = createdAt; 
+    }
 
-    public List<VisitLog> getVisitLogs() { return visitLogs; }
+    public List<Appointment> getAppointments(){
+         return appointments; 
+    }
+    public void setAppointments(List<Appointment> appointments){
+         this.appointments = appointments; 
+    }
+
+    public List<VisitLog> getVisitLogs(){
+         return visitLogs; }
     public void setVisitLogs(List<VisitLog> visitLogs) { this.visitLogs = visitLogs; }
 }
