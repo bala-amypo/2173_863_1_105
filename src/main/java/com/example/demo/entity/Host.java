2fde6abdbd2d31f8a48eb56b71f8204921132a66
bala-amypo <bala@amypo.in>
2025-12-21 07:32:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/Host.java
+++ src/main/java/com/example/demo/entity/Host.java
@@ -61,22 +61,42 @@ public class Host {
        this.id = id; 
     }
 
-    public String getHostName() { return hostName; }
-    public void setHostName(String hostName) { this.hostName = hostName; }
+    public String getHostName(){
+     return hostName; 
+     }
+    public void setHostName(String hostName){ 
+        this.hostName = hostName; 
+    }
 
-    public String getFullname() { return fullname; }
-    public void setFullname(String fullname) { this.fullname = fullname; }
+    public String getFullname(){
+         return fullname; 
+    }
+    public void setFullname(String fullname){
+         this.fullname = fullname; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail(){
+         return email; 
+    }
+    public void setEmail(String email){
+         this.email = email; 
+    }
 
-    public String getDepartment() { return department; }
-    public void setDepartment(String department) { this.department = department; }
+    public String getDepartment(){
+         return department; 
+    }
+    public void setDepartment(String department){
+         this.department = department; 
+    }
 
-    public String getPhone() { return phone; }
-    public void setPhone(String phone) { this.phone = phone; }
+    public String getPhone(){ 
+        return phone; 
+    }
+    public void setPhone(String phone){
+         this.phone = phone; 
+    }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getCreatedAt(){ return createdAt; }
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
 
     public List<Appointment> getAppointments() { return appointments; }
