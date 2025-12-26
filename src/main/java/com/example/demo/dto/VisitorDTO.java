e54bbb3433b48dc20698216b97cd87544a873626
bala-amypo <bala@amypo.in>
2025-12-26 04:21:32 +0000

    User commit

--- src/main/java/com/example/demo/dto/VisitorDTO.java
+++ src/main/java/com/example/demo/dto/VisitorDTO.java
@@ -1,16 +1,43 @@
 package com.example.demo.dto;
 
-import lombok.*;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class VisitorDTO {
 
-    private Long id;
     private String fullName;
-    private String phone;
     private String email;
+    private String phone;
     private String idProofNumber;
+
+    public VisitorDTO() {}
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getIdProofNumber() {
+        return idProofNumber;
+    }
+
+    public void setIdProofNumber(String idProofNumber) {
+        this.idProofNumber = idProofNumber;
+    }
 }
--- src/main/java/com/example/demo/repository/VisitorRepository.java
+++ src/main/java/com/example/demo/repository/VisitorRepository.java
@@ -3,9 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface VisitorRepository extends JpaRepository<Visitor, Long> {
-
-    Optional<Visitor> findByEmail(String email);
 }
