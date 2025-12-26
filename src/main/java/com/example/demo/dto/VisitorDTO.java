efc2dd9bb1fab01e46244826bacbef296987ad17
bala-amypo <bala@amypo.in>
2025-12-26 04:24:32 +0000

    User commit

--- src/main/java/com/example/demo/dto/VisitorDTO.java
+++ src/main/java/com/example/demo/dto/VisitorDTO.java
@@ -2,12 +2,31 @@ package com.example.demo.dto;
 
 public class VisitorDTO {
 
+    private Long id;
     private String fullName;
-    private String email;
     private String phone;
+    private String email;
     private String idProofNumber;
 
-    public VisitorDTO() {}
+    public VisitorDTO() {
+    }
+
+    public VisitorDTO(Long id, String fullName, String phone, String email, String idProofNumber) {
+        this.id = id;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+        this.idProofNumber = idProofNumber;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getFullName() {
         return fullName;
@@ -17,20 +36,20 @@ public class VisitorDTO {
         this.fullName = fullName;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getPhone() {
         return phone;
     }
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getIdProofNumber() {
